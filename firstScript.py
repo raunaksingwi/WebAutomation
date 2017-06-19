c8aa228edd2ba1c6ea2e51e191d3f86dca8d0a3b
Raunak Singwi <raunaksingwi7@gmail.com>
2017-06-19 15:08:47 +0530

    Change from Safari to Chrome

--- firstScript.py
+++ firstScript.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Safari()
+driver = webdriver.Chrome()
 driver.get("https://www.google.com")
 elem = driver.find_element_by_name("q")
 elem.send_keys("automation is cool")
